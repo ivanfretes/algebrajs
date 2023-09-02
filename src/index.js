c58ab0d6611de03b027550416e1ab30cce95e9ed
Ivan <vanfretes@gmail.com>
2023-09-02 10:25:11 -0400

    feat: getSlopeAngle2 and getDistanceBetweenTwoPoints

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-
-
 /**
- * @param point1 - array i.e [1,2] - Init point of ecuation
- * @param point2 - array i.e [3,4] - End point of ecuation
+ * @param {array} point1 # i.e [1,2] - Init point of ecuation
+ * @param {array} point2 i.e [3,4] - End point of ecuation
  * 
- * @return  ecuation =>  [myX, ,mxY, constant]
+ * @return {array} ecuation =>  [myX, ,mxY, constant]
  */
 export const genLinearEcuation = (point1,point2) => {
    const [x1, y1] = point1;
@@ -16,9 +14,14 @@ export const genLinearEcuation = (point1,point2) => {
    return [my, mx, constant]
 }
 
-
 /**
+ * Get the slop value
+ * @param {number} x1
+ * @param {number} y1
+ * @param {number} x2
+ * @param {number} y2 
  * 
+ * @return {Object} = with slote m, mx and my
  */
 export const genSlopeValues = (x1,y1,x2,y2) => {
    const my = y2 - y1
@@ -27,7 +30,13 @@ export const genSlopeValues = (x1,y1,x2,y2) => {
    return {m, mx, my}
 }
 
-
+/**
+ * Return the point of intersention in two Straigh / linear ecuation
+ * @param {array} -r1 
+ * @param {array} -r2
+ * 
+ * @return {array} [ x, y ]
+ */
 export const getPointOfIntersection = (r1, r2) => {
    const [x1,y1,c1] = r1.map(element => element * r2[0])
    const [x2,y2,c2] = r2.map(element => element * -r1[0])
@@ -39,19 +48,48 @@ export const getPointOfIntersection = (r1, r2) => {
 
 /**
  * From the formula m = -a / b
- * @param {Array} r1 - Straigh / linear ecuation
+ * @param {array} r -Straigh / linear ecuation
  * 
- * @return {Number} - slope
+ * @return {number} - slope
  */
-export const getStraighSlope = (r1) => {
-   return - r1[0] / r1[1]
+export const getStraighSlope = ([x,y]) => {
+   return - x / y
 }
 
 /**
+ * Get slope angle with slope value
+ * @param {number} slopeValue
  * 
+ * @return {number}
  */
-export const getSlopeAngle = () => 
+export const getSlopeAngle = (slopeValue) => {
+   const s = (Math.atan(slopeValue) * 180) / Math.PI
+   return s < 0 ? 180 + s : s
+}
 
+/**
+ * Get slope angle with x and y params
+ * 
+ * @param {number} x
+ * @param {number} y
+ * 
+ * @return {number}
+ */
+export const getSlopeAngle2 = (x, y) => {
+   return (Math.atan2(y, x) * 180) / Math.PI
+}
+
+/**
+ * Get the distance between two points
+ * 
+ * @param {number} x - only one value or substrac operation (x1 - x2) 
+ * @param {number} y - only one value or substrac operation (y1 - y2) 
+ * 
+ * @return {number}
+ */
+export const getDistanceBetweenTwoPoints = (x, y) => {
+   return Math.sqrt(Math.pow(x,2) + Math.pow(y,2));
+}
 
 export default {
    genLinearEcuation,
